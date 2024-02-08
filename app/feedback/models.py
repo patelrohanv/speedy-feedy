f6e239bcebc32106900908137b803b60cc25c8af
Tramayne Watson <tramayne33@yahoo.com>
2024-02-07 18:12:06 -0800

    adding grow model and removing dups

--- app/feedback/models.py
+++ app/feedback/models.py
@@ -1,55 +1,42 @@
 from django.db import models
 from employees.models import Employee
 
-
-class SBI_Feedback(models.Model):
-    situation = models.TextField()
-    behavior = models.TextField()
-    impact = models.TextField()
+class Feedback(models.Model):
     provider = models.ForeignKey(
-        Employee, related_name="sbi_feedback_provided", on_delete=models.CASCADE
+        Employee, 
+         related_name="%(class)s_provided_feedback"
+         ,on_delete=models.CASCADE
     )
     receiver = models.ForeignKey(
-        Employee, related_name="sbi_feedback_received", on_delete=models.CASCADE
+        Employee, 
+        related_name="%(class)s_received_feedback",
+        on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
-        return f"SBI Feedback from {self.provider} to {self.receiver}"
+        return f"Feedback from {self.provider} to {self.receiver}"
 
 
-class DESC_Feedback(models.Model):
+class SBI_Feedback(Feedback):
+    situation = models.TextField()
+    behavior = models.TextField()
+    impact = models.TextField()
+
+
+class DESC_Feedback(Feedback):
     describe = models.TextField()
     explain = models.TextField()
     specify = models.TextField()
     consequences = models.TextField()
-    provider = models.ForeignKey(
-        Employee, related_name="desc_provided_feedback", on_delete=models.CASCADE
-    )
-    receiver = models.ForeignKey(
-        Employee, related_name="desc_received_feedback", on_delete=models.CASCADE
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"DESC Feedback from {self.provider} to {self.receiver}"
 
 
-# class GROW_Feedback(models.Model):
-#     goal = models.TextField()
-#     reality = models.TextField()
-#     options = models.TextField()
-#     will = models.TextField()
-# provider = models.ForeignKey(
-#     Employee, related_name='grow_provided_feedback', on_delete=models.CASCADE
-# )
-# receiver = models.ForeignKey(
-#     Employee, related_name='grow_received_feedback', on_delete=models.CASCADE
-# )
-# created_at = models.DateTimeField(auto_now_add=True)
-# updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return f"GROW Feedback from {self.provider} to {self.receiver}"
+class GROW_Feedback(Feedback):
+    goal = models.TextField()
+    reality = models.TextField()
+    options = models.TextField()
+    will = models.TextField()
